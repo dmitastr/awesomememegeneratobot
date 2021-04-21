10ccf7df4f32f6c6741b00b7f7998cb2d7cb52ff
dmitastr <d.astretsov@gmail.com>
2021-04-21 15:14:29 +0300

    remove polling

--- meme_generator_bot/meme_generator_bot.py
+++ meme_generator_bot/meme_generator_bot.py
@@ -301,7 +301,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=BOT_TOKEN)
     updater.bot.setWebhook('https://mem-generator.herokuapp.com/' + BOT_TOKEN)
 
-    updater.start_polling()
+    # updater.start_polling()
     updater.idle()
 
 
